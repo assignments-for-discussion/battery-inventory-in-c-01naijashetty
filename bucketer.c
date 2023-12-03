29a5bf9671d9a92e0b920845f1d6e93225559a6a
NAIJA SHETTY <135134271+01naijashetty@users.noreply.github.com>
2023-12-03 17:50:42 +0530

    Update bucketer.c

--- bucketer.c
+++ bucketer.c
@@ -48,6 +48,32 @@ void testBucketingByHealth() {
   printf("\nCounts: Healthy = %d, Exchange = %d, Failed = %d\n", counts.healthy, counts.exchange, counts.failed);
   printf("Done counting :)\n");
   
+  //All batteries failed
+  const int presentCapacities3[] = {50, 40, 30, 20, 10};
+  const int numberOfBatteries3 = sizeof(presentCapacities3) / sizeof(presentCapacities3[0]);
+  struct CountsBySoH counts3 = countBatteriesByHealth(presentCapacities3, numberOfBatteries3);
+  printf("\nCounts: Healthy = %d, Exchange = %d, Failed = %d\n", counts3.healthy, counts3.exchange, counts3.failed);
+  
+  //empty battery array
+  const int presentCapacities4[] = {};
+  const int numberOfBatteries4 = 0;
+  struct CountsBySoH counts4 = countBatteriesByHealth(presentCapacities4, numberOfBatteries4);
+  printf("\nCounts: Healthy = %d, Exchange = %d, Failed = %d\n", counts4.healthy, counts4.exchange, counts4.failed);
+  
+  // Boundary condition - maximum rated capacity
+  const int presentCapacities5[] = {120, 120, 120, 120};
+  const int numberOfBatteries5 = sizeof(presentCapacities5) / sizeof(presentCapacities5[0]);
+  struct CountsBySoH counts5 = countBatteriesByHealth(presentCapacities5, numberOfBatteries5);
+  printf("\nCounts: Healthy = %d, Exchange = %d, Failed = %d\n", counts5.healthy, counts5.exchange, counts5.failed);
+  
+  // Boundary condition - all batteries with minimum rated capacity
+  const int presentCapacities6[] = {1, 1, 1, 1};
+  const int numberOfBatteries6 = sizeof(presentCapacities6) / sizeof(presentCapacities6[0]);
+  struct CountsBySoH counts6 = countBatteriesByHealth(presentCapacities6, numberOfBatteries6);
+  printf("\nCounts: Healthy = %d, Exchange = %d, Failed = %d\n", counts6.healthy, counts6.exchange, counts6.failed);
+  
+}
+  
 }
 
 int main() {
